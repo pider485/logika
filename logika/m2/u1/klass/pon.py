7a46ffd6272fd6e11d95f58753d365caf1297d6c
pider485 <84933632+pider485@users.noreply.github.com>
2023-07-28 16:39:43 +0000

    Add files via upload

--- logika/m2/u1/klass/pon.py
+++ logika/m2/u1/klass/pon.py
@@ -1,4 +1,18 @@
-i=0
-while True:
-    i+=1
-    print(i,"sany loh")
+class Button():
+    def __init__(self, title_text, x_num, y_num):
+        self.title = title_text
+        self.x = x_num
+        self.y = y_num
+        self.appearance = True
+    def hide(self):
+        self.appearance = False
+    def show(self):
+        self.appearance = True
+    def print_status(self):
+        print('Дані про віджет:')
+        print(self.title, self.x, self.y, self.appearance)
+
+ok_button = Button('ok', 100, 100)
+ok_button.print_status()
+ok_button.hide()
+ok_button.print_status()
